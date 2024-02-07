a5577c009d921e34385afdbb28dd6d0f4fb3cc0b
Liubomyr Yarych <78175599+yarychok@users.noreply.github.com>
2024-02-07 18:48:18 +0200

    Adjusted Vite config to fix the env variable issue on prod.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [{ find: "@", replacement: path.resolve(__dirname, "src")}],
-  },
-  define: {
-    __APP_ENV__: process.env.VITE_VERCEL_ENV,
-  }
-})
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: [{ find: "@", replacement: path.resolve(__dirname, "src")}],
+//   },
+// })
+
+export default ({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return defineConfig({
+    plugins: [
+      react(),
+    ],
+    resolve: {
+      alias: [{ find: "@", replacement: path.resolve(__dirname, "src")}],
+    },
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+  })
+}
